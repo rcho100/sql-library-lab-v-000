37cbdd4cebdb1f08208a72fa1db99741daf8b1cf
Richard Cho <richardcho100@gmail.com>
2018-10-19 23:21:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'humans' GROUP BY series.title;"
+  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'humans' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
